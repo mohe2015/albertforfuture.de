72ed91010ad6dc93b82287d00280160c3554372f
mohe2015 <mohe2015@users.noreply.github.com>
2019-12-17 20:06:20 +0100

    Start working on service worker

---
+++ themes/albertforfuture.de/static/index.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js', {scope: './'})
+  .then((reg) => {
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch((error) => {
+    console.log('Registration failed with ' + error);
+  });
+}
