f8617dbb564b6bcdd9f68a70704ea85d87c7c31d
mohe2015 <mohe2015@users.noreply.github.com>
2019-12-18 15:03:52 +0000

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+hugo --minify
+#find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec gzip -v -k -f --best {} \;
+find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec zopfli -v -i1000 {} \;
