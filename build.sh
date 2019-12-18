ace5dc09273d7613415833f99b35ff8d7a9634a0
mohe2015 <mohe2015@users.noreply.github.com>
2019-12-18 15:57:56 +0000

    new idea

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+set -ex
 hugo --minify
 #find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec gzip -v -k -f --best {} \;
-find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec zopfli -v -i1000 {} \;
+find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec zopfli -v -i1 {} \;
