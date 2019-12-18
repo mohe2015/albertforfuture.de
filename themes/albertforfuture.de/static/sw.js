33ef767e5bfa9de7ae517ee548652ba854ab669d
mohe2015 <mohe2015@users.noreply.github.com>
2019-12-18 17:43:23 +0100

    Service worker dont load index.js

--- themes/albertforfuture.de/static/sw.js
+++ themes/albertforfuture.de/static/sw.js
@@ -4,8 +4,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        './index.html',
-        "./index.js"
+        './index.html'
       ]);
     })
   );
