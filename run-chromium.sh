80cef4f06ae868e367c356da54fddbc17d1e5be2
mohe2015 <mohe2015@users.noreply.github.com>
2019-12-21 17:56:35 +0100

    Update

--- run-chromium.sh
+++ run-chromium.sh
@@ -1,1 +1,1 @@
-chromium --user-data-dir=/tmp/chrome --enable-quic --quic-host-whitelist="localhost" --origin-to-force-quic-on=localhost:443
+google-chrome-unstable --user-data-dir=/tmp/chrome --enable-quic --quic-host-whitelist="beta.albertforfuture.de" --origin-to-force-quic-on=beta.albertforfuture.de:443
