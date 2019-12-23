3acb7f91cd3c7229cee53a7db13a3c48f371ab74
mohe2015 <mohe2015@users.noreply.github.com>
2019-12-23 19:39:42 +0100

    Small fixes

--- themes/albertforfuture.de/assets/sw_template.js
+++ themes/albertforfuture.de/assets/sw_template.js
@@ -10,7 +10,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        {{ (resources.Get "main.css" | toCSS | minify | fingerprint).RelPermalink }},
+        '{{ (resources.Get "main.css" | toCSS | minify | fingerprint).RelPermalink }}',
         '{{ (resources.Get "logo.svg" | fingerprint).RelPermalink }}',
 
         '/sw.js',
