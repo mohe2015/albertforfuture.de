61fc14679a1623abbbc2a5ee004ae47e1bf88b28
dev.mohe <Moritz.Hedtke@t-online.de>
2020-01-26 21:21:04 +0100

    Don't minfy

--- themes/albertforfuture.de/assets/sw_template.js
+++ themes/albertforfuture.de/assets/sw_template.js
@@ -17,7 +17,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v6').then((cache) => {
       return cache.addAll([
-        '{{ (resources.Get "custom.scss" | toCSS | minify | fingerprint).RelPermalink }}',
+        '{{ (resources.Get "custom.scss" | toCSS | fingerprint).RelPermalink }}',
         '{{ (resources.Get "logo.svg" | fingerprint).RelPermalink }}',
 
         '/sw.js',
