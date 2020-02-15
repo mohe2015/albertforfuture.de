6af52cf2981fb36be7d97e3968ee301c55720252
dev.mohe <Moritz.Hedtke@t-online.de>
2020-02-15 17:25:36 +0100

    Update map

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 set -ex
-git pull
+# git pull
 hugo --minify --baseURL $1
-find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec zopfli -v -i1000 {} \;
+#find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec zopfli -v -i15 {} \;
