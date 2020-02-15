0fc5148dbd0ac3e3396aff0c41d13732c3b4387b
dev.mohe <Moritz.Hedtke@t-online.de>
2020-02-15 17:46:12 +0100

    Small bugfix

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 set -ex
 # git pull
 hugo --minify --baseURL $1
-#find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec zopfli -v -i15 {} \;
+# find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec zopfli -v -i15 {} \;
+# sudo systemctl restart nginx
