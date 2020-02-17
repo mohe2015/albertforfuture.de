2cd5d1aebbb00fee74dc9674d5be8e0e50e26396
dev.mohe <Moritz.Hedtke@t-online.de>
2020-02-17 22:07:55 +0100

    Some fixes

--- web-push/src/main.rs
+++ web-push/src/main.rs
@@ -10,8 +10,7 @@ use dotenv::dotenv;
 use std::env;
 use self::models::*;
 
-use argparse::{ArgumentParser, Store, StoreOption};
-use std::{fs::File, io::Read};
+use std::{fs::File};
 use web_push::*;
 
 use warp::Filter;
@@ -29,21 +28,7 @@ pub fn establish_connection() -> SqliteConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
-use serde_derive::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct SubscriberKeysJson {
-    pub p256dh: String,
-    pub auth: String
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct SubscriberJson {
-    pub endpoint: String,
-    pub keys: SubscriberKeysJson
-}
-
-pub async fn subscribe(subscriber: SubscriberJson) -> Result<impl warp::Reply, Infallible> {
+pub async fn subscribe(subscriber: SubscriptionInfo) -> Result<impl warp::Reply, warp::Rejection> {
     println!("{} {} {}", subscriber.endpoint, subscriber.keys.p256dh, subscriber.keys.auth);
     
     let connection = establish_connection();
@@ -54,6 +39,8 @@ pub async fn subscribe(subscriber: SubscriberJson) -> Result<impl warp::Reply, I
         key_auth: &subscriber.keys.auth
     };
 
+    let _test = send_notification(&subscriber).await;
+
     diesel::insert_or_ignore_into(subscribers::table)
         .values(&new_subscriber)
         .execute(&connection)
@@ -63,7 +50,7 @@ pub async fn subscribe(subscriber: SubscriberJson) -> Result<impl warp::Reply, I
 }
 
 
-pub async fn unsubscribe(subscriber: SubscriberJson) -> Result<impl warp::Reply, Infallible> {
+pub async fn unsubscribe(subscriber: SubscriptionInfo) -> Result<impl warp::Reply, warp::Rejection> {
     println!("{} {} {}", subscriber.endpoint, subscriber.keys.p256dh, subscriber.keys.auth);
     
     let connection = establish_connection();
@@ -115,87 +102,17 @@ fn main1() {
 
 // BAvD4b287z3xfU293G2JSKXybiHv-19mNhzlvQmmDk9drnsWhPpeSC6d9uCThC4y4abw4gjyxA8YX9Z7rk4PfvI=
 
-#[tokio::main]
-async fn main2() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let mut subscription_info_file = String::new();
-    let mut gcm_api_key: Option<String> = None;
-    let mut vapid_private_key: Option<String> = None;
-    let mut push_payload: Option<String> = None;
-    let mut ttl: Option<u32> = None;
-
-    {
-        let mut ap = ArgumentParser::new();
-        ap.set_description("A web push sender");
-
-        ap.refer(&mut gcm_api_key).add_option(
-            &["-k", "--gcm_api_key"],
-            StoreOption,
-            "Google GCM API Key",
-        );
-
-        ap.refer(&mut vapid_private_key).add_option(
-            &["-v", "--vapid_key"],
-            StoreOption,
-            "A NIST P256 EC private key to create a VAPID signature",
-        );
-
-        ap.refer(&mut subscription_info_file)
-            .add_option(&["-f", "--subscription_info_file"], Store,
-                        "Subscription info JSON file, https://developers.google.com/web/updates/2016/03/web-push-encryption");
-
-        ap.refer(&mut push_payload).add_option(
-            &["-p", "--push_payload"],
-            StoreOption,
-            "Push notification content",
-        );
-
-        ap.refer(&mut ttl).add_option(
-            &["-t", "--time_to_live"],
-            StoreOption,
-            "TTL of the notification",
-        );
-
-        ap.parse_args_or_exit();
-    }
-
-    let mut file = File::open(subscription_info_file).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-
-    let subscription_info: SubscriptionInfo = serde_json::from_str(&contents).unwrap();
-
-    let mut builder = WebPushMessageBuilder::new(&subscription_info).unwrap();
-
-    if let Some(ref payload) = push_payload {
-        builder.set_payload(ContentEncoding::AesGcm, payload.as_bytes());
-    }
-
-    if let Some(ref gcm_key) = gcm_api_key {
-        builder.set_gcm_key(gcm_key);
-    }
-
-    if let Some(time) = ttl {
-        builder.set_ttl(time);
-    }
-
-    if let Some(ref vapid_file) = vapid_private_key {
-        let file = File::open(vapid_file).unwrap();
-
-        let mut sig_builder = VapidSignatureBuilder::from_pem(file, &subscription_info).unwrap();
-
-        sig_builder.add_claim("sub", "mailto:test@example.com");
-        sig_builder.add_claim("foo", "bar");
-        sig_builder.add_claim("omg", 123);
-
-        let signature = sig_builder.build().unwrap();
-
-        builder.set_vapid_signature(signature);
-    };
-
+async fn send_notification(subscription: &SubscriptionInfo) -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let mut builder = WebPushMessageBuilder::new(&subscription).unwrap();
+    builder.set_payload(ContentEncoding::AesGcm, "test".as_bytes());
+    builder.set_gcm_key("BAvD4b287z3xfU293G2JSKXybiHv-19mNhzlvQmmDk9drnsWhPpeSC6d9uCThC4y4abw4gjyxA8YX9Z7rk4PfvI=");
+    let file = File::open("./private_key.pem").unwrap();
+    let mut sig_builder = VapidSignatureBuilder::from_pem(file, &subscription).unwrap();
+    sig_builder.add_claim("sub", "mailto:test@example.com");
+    let signature = sig_builder.build().unwrap();
+    builder.set_vapid_signature(signature);
     let client = WebPushClient::new();
-
-    let response = client.send(builder.build()?).await?;
-    println!("Sent: {:?}", response);
-
+    let built = builder.build().unwrap();
+    let response = client.send(built).await;
     Ok(())
 }
