b50d02ea3e8ad9bb3ae812ddf8e221271b555527
dev.mohe <Moritz.Hedtke@t-online.de>
2020-02-22 22:57:39 +0100

    Small fixes

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 export default {
     input: 'themes/albertforfuture.de/assets/index.js',
     output: {
-      file: 'bundle.js',
+      file: 'themes/albertforfuture.de/assets/bundle.js',
       format: 'esm'
     },
     plugins: [
