ae148c047f5c78702a71cf204b445996fe9e0085
dev.mohe <Moritz.Hedtke@t-online.de>
2020-02-23 22:19:31 +0100

    Don't push the javascript as it isn't required. At least when the navbar is css-only

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 export default {
     input: 'public/index.js',
     output: {
-      file: 'public/bundle.js'
+      file: 'public/bundle.js',
     },
     plugins: [
       resolve()
